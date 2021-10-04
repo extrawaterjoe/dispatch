8fed7b7dc6158648f632d184e14f8f596f0bda1a
joe lazarz <jlazarz87@gmail.com>
2021-10-04 14:43:12 -0400

    plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,25 @@ module.exports = {
     author: `@extrawaterjo`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `dispatch`,
+        short_name: `dispatch`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from "react"
+import styled from "styled-components"
+
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+const Styledh1 = styled.h1`
+  color: purple;
+`
 
 export default function Home() {
   return (
     <Layout>
       <Seo title="Home" />
-      <div>Hello world!</div>
+      <Styledh1>Hello world!</Styledh1>
     </Layout>
   )
 }
