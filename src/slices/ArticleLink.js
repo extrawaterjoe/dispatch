d52269a66ac37224ebb533af2efe4883cafba614
joe lazarz <jlazarz87@gmail.com>
2021-10-23 16:14:16 -0400

    Article Link component

--- src/slices/ArticleLink.js
+++ src/slices/ArticleLink.js
@@ -1,11 +1,29 @@
 import React from "react"
+import styled from "styled-components"
+import "../styles/css-vars.css"
+
+const LinkButton = styled.a`
+  border: 2px solid var(--text-color);
+  padding: 10px 20px;
+  width: 100%;
+  text-align: center;
+  text-decoration: none;
+  color: var(--text-color);
+  display: inline-block;
+
+  &:hover,
+  &:active {
+    background-color: var(--text-color);
+    color: var(--background-color);
+  }
+`
 
 const ArticleLink = ({ slice }) => {
-  console.log("Link:", slice)
   return (
     <>
-      <div>{slice.primary.link_text.text}</div>
-      <div>{slice.primary.url.url}</div>
+      <LinkButton href={slice.primary.url.url}>
+        {slice.primary.link_text.text}
+      </LinkButton>
     </>
   )
 }
